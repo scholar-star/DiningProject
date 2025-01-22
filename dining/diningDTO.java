ea649e3a27550db8d370333a931b08517b0e11c5
scholar-star <cheve1224@pusan.ac.kr>
2025-01-22 18:25:25 +0900

    식당 DTO 생성

---
+++ dining/diningDTO.java
@@ -0,0 +1,31 @@
+package busan.restaurants.domain.dining;
+
+public class diningDTO {
+    private String name;
+    private String location;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
